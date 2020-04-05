4f4ca2fa620c66204c3d84ec90daec5d5db3c196
R.Kawasaki <gettodowithallhaste9@gmail.com>
2020-04-05 07:12:45 +0000

    Re-create top page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,7 @@ class ApplicationController < ActionController::Base
   
   def require_user_logged_in
     unless logged_in?
-<<<<<<< HEAD
-      redirect_to root_url
-=======
-      #redirect_to root_url
->>>>>>> 36d4dcca1ff95ac44c308d339fed8d1465374f35
+      redirect_to controller: :toppages, action: :index
     end
   end
-  
 end
